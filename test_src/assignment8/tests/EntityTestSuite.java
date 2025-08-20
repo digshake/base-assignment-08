75c0168b5811ad4de7e617749b48692aa33a1b7e
Doug Shook <dshook@wustl.edu>
2025-08-20 12:14:07 -0500

    Removing tests as import no longer functions

--- test_src/assignment8/tests/EntityTestSuite.java
+++ test_src/assignment8/tests/EntityTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ EntityConstructorTest.class, EntityRadiusTest.class, EntityDistanceCenterToPointTest.class,
+@Suite.SuiteClasses({ EntityConstructorTest.class, EntityDistanceCenterToPointTest.class,
 		EntityEdgeToEdgeAndIsTouchingTest.class, EntityMoveTowardTest.class, EntityMoveAwayFromTest.class })
 public class EntityTestSuite {
 }
